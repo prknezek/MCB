13d6f2fd17526428f2e331e5dfdf79bee33ef97e
prknezek <prknezek@gmail.com>
2023-10-10 00:36:25 -0500

    Encoded and decoded chess pieces and squares

--- 0x88_board.cpp
+++ 0x88_board.cpp
@@ -16,13 +16,46 @@ using std::string;
 // *uppercase* = white piece
 // *lowercase* = black piece
 // o = off board scores
-enum {e, P, N, B, R, Q, K, p, n, b, r, q, k, o};
+enum pieces {e, P, N, B, R, Q, K, p, n, b, r, q, k, o};
+
+// square encoding
+enum squares {
+    a8 = 0, b8, c8, d8, e8, f8, g8, h8,
+    a7 = 16, b7, c7, d7, e7, f7, g7, h7,
+    a6 = 32, b6, c6, d6, e6, f6, g6, h6,
+    a5 = 48, b5, c5, d5, e5, f5, g5, h5,
+    a4 = 64, b4, c4, d4, e4, f4, g4, h4,
+    a3 = 80, b3, c3, d3, e3, f3, g3, h3,
+    a2 = 96, b2, c2, d2, e2, f2, g2, h2,
+    a1 = 112, b1, c1, d1, e1, f1, g1, h1
+};
 
 // ascii pieces
 char ascii_pieces[] = ".PNBRQKpnbrqko";
 
 // unicode pieces
-string unicode_pieces[] = {".  ", "♙", "♘", "♗", "♖", "♕", "♔", "♟︎", "♞", "♝", "♜", "♛", "♚"};
+string unicode_pieces[] = {". ", "♙", "♘", "♗", "♖", "♕", "♔", "♟︎", "♞", "♝", "♜", "♛", "♚"};
+
+// encode ascii pieces
+int char_pieces[128];
+
+void initialize_char_pieces() {
+    for (int i = 0; i < 128; ++i) {
+        char_pieces[i] = e;
+    }
+    char_pieces['P'] = P;
+    char_pieces['N'] = N;
+    char_pieces['B'] = B;
+    char_pieces['R'] = R;
+    char_pieces['Q'] = Q;
+    char_pieces['K'] = K;
+    char_pieces['p'] = p;
+    char_pieces['n'] = n;
+    char_pieces['b'] = b;
+    char_pieces['r'] = r;
+    char_pieces['q'] = q;
+    char_pieces['k'] = k;
+}
 
 // chess board representation
 int board[128] = {
@@ -34,6 +67,18 @@ int board[128] = {
     e, e, e, e, e, e, e, e,  o, o, o, o, o, o, o, o,
     P, P, P, P, P, P, P, P,  o, o, o, o, o, o, o, o,
     R, N, B, Q, K, B, N, R,  o, o, o, o, o, o, o, o
+};
+
+// convert board square indexes to coords
+string square_to_coords[] = {
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8", "i8", "j8", "k8", "l8", "m8", "n8", "o8", "p8",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7", "i7", "j7", "k7", "l7", "m7", "n7", "o7", "p7",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6", "i6", "j6", "k6", "l6", "m6", "n6", "o6", "p6",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5", "i5", "j5", "k5", "l5", "m5", "n5", "o5", "p5",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4", "i4", "j4", "k4", "l4", "m4", "n4", "o4", "p4",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3", "i3", "j3", "k3", "l3", "m3", "n3", "o3", "p3",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", "i2", "j2", "k2", "l2", "m2", "n2", "o2", "p2",
+    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1", "i1", "j1", "k1", "l1", "m1", "n1", "o1", "p1"
 };
 
 // print board
@@ -62,11 +107,13 @@ void print_board() {
         cout << endl;
     }
     // print files
-    cout << "\n   a b c d e f g h\n";
+    cout << "\n   a  b  c  d  e  f  g  h\n";
 }
 
 // main driver
 int main() {
+    initialize_char_pieces();
+    
     print_board();
     return 0;
 }
